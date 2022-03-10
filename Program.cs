db68f58a3cadd177d56d699c86f919637eeea037
Liubov K <lyouso@yandex.ru>
2022-03-10 13:53:36 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+for (int i = n; i <= number; i++){
+    if(i % 2 == 0){
+        Console.WriteLine(i);
+    }
+}
